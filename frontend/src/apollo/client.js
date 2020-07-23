4cc4c004381866c475386f93f8ba101cc4a7b85f
ronaldsfcruz <ronald.cruz@afya.com.br>
2020-07-23 18:40:06 -0300

    requests https

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -4,7 +4,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: "http://gatsby-dynamic.herokuapp.com/graphql",
+    uri: "https://gatsby-dynamic.herokuapp.com/graphql",
     fetch,
   }),
 })
