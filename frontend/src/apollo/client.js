c21ed4d00ef325162da9f439322799a241cb790b
ronaldsfcruz <ronald.cruz@afya.com.br>
2020-07-23 17:55:59 -0300

    added static production url

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -4,7 +4,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: process.env.API_URL + "/graphql",
+    uri: "http://gatsby-dynamic.herokuapp.com/graphql",
     fetch,
   }),
 })
